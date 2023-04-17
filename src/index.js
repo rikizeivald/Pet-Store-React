56a19e4aade508482beea0cac47265f5ae75cc25
rikizeivald <86789273+rikizeivald@users.noreply.github.com>
2023-04-17 03:49:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import {BrowserRouter as Router}from 'react-router-dom'
 import { petReducer } from './reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store=createStore(combineReducers({pet:petReducer}),composeEnhancers(applyMiddleware(thunk)))
+//store.dispatch(getAllPets());
 
 ReactDOM.render(
   <React.StrictMode>
